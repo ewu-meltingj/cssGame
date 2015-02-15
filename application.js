40eaca098b55c7f3ec163baddf84e617d4bfcb9c
ewu-meltingj <jackthetron@eagles.ewu.edu>
2015-02-14 19:18:07 -0800

    added loader

--- application.js
+++ application.js
@@ -1,5 +1,3 @@
-
-
 var canvasBack = document.getElementById('background'),
 ctxBack = canvasBack.getContext('2d');
 window.addEventListener('resize', resizeCanvas, false);
@@ -16,21 +14,22 @@ function render() {
 
 var view = new View();
 var observer = new Observer(view);
+
 var world = new World("Paradise", 500, 500);
 var mainCharacter = new Person("Jeremy", 20, 20, 30, 30, 1);
-world.addEntity(new Room(100, 200, 10, 10, 1));
 
+world.addEntity(new Room(100, 200, 10, 10, 1));
 world.addEntity(new Room(200, 200, 120, 10, 1));
+world.addEntity(new Room(100, 200, 380, 10, 1));
 
 world.addEntity(mainCharacter);
-
 observer.addEntity(world);
 
 
 var controller = new Control(mainCharacter, world);
 
 resizeCanvas();
-setInterval(onTimerTick, 33); // 33 milliseconds = ~ 30 frames per sec
+setInterval(onTimerTick, 33);
 
 function onTimerTick() {
     observer.update();
